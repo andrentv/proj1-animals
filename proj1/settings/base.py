bff36eb8c2f9edba00515cbabf0a03daefca6544
andrentv <comercial@vaccaros.com.br>
2021-05-14 15:00:16 -0300

    main

--- proj1/settings/base.py
+++ proj1/settings/base.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'proj1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "animals",
+        'USER': "admin",
+        'PASSWORD': "123456",
+        'HOST': "127.0.0.1",
+        'PORT': 5432,
     }
 }
 
