4b4762367c188b95d4ab2ab7ba1f09ee20e76487
andrentv <comercial@vaccaros.com.br>
2021-05-16 17:05:53 -0300

    semmidia

--- proj1/settings/base.py
+++ proj1/settings/base.py
@@ -133,8 +133,8 @@ STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles")
 
 
 # django_on_heroku.settings(locals())
-# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
